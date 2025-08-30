39220e5c5dd753cdc5f3d78a475f84d14309e34b
Ritikraj3 <ritikraj5776@gmail.com>
2025-08-31 00:35:08 +0530

    list

---
+++ 6_Python-List/list.py
@@ -0,0 +1,14 @@
+# The list is changeable, meaning that 
+# we can change, add, and remove items in a list 
+# after it has been created.
+myList = ["apple", "banana", "cherry"]
+
+print(myList[0])
+myList[0] = "orange"
+print(myList[0])
+print(myList[0:])
+print(myList[0:3])
+print(myList[0:len(myList)])
+
+thislist = list(("book", "pen", "notebook", "pencil"))
+print(thislist)
